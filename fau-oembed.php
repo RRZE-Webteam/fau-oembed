8972f756e179740e838f0552a2a9ac270fa0fc89
rvdforst <rolf.v.d.forst@fau.de>
2022-10-21 15:17:07 +0200

    Update fau-oembed.php

--- fau-oembed.php
+++ fau-oembed.php
@@ -1,120 +1,158 @@
 <?php
 
-/**
- * Plugin Name:         FAU oEmbed
- * Plugin URI:          https://github.com/RRZE-Webteam/fau-oembed
- * GitHub Plugin URI:   https://github.com/RRZE-Webteam/fau-oembed
- * Description:         Automatische Einbindung der FAU-Karten, Videos von FAU.tv, YouTube-Videos ohne Cookies, sowie weitere oEmbed-Quellen der FAU.
- * Version:             3.2.3
- * Author:              RRZE-Webteam
- * Author URI:          https://blogs.fau.de/webworking/
- * License:             GNU General Public License v2
- * License URI:         http://www.gnu.org/licenses/gpl-2.0.html
- * Domain Path:         /languages
- * Text Domain:         fau-oembed
- */
-
-
-
-
+/*
+Plugin Name:         FAU oEmbed
+Plugin URI:          https://github.com/RRZE-Webteam/fau-oembed
+GitHub Plugin URI:   https://github.com/RRZE-Webteam/fau-oembed
+Description:         Automatic integration of FAU maps, videos from FAU.tv, YouTube videos without cookies, and other FAU oEmbed sources.
+Version:             3.3.0
+Author:              RRZE-Webteam
+Author URI:          https://blogs.fau.de/webworking/
+License:             GNU General Public License v2
+License URI:         http://www.gnu.org/licenses/gpl-2.0.html
+Domain Path:         /languages
+Text Domain:         fau-oembed
+*/
 
 namespace FAU\OEmbed;
 
-
 defined('ABSPATH') || exit;
 
-const RRZE_PHP_VERSION = '7.3';
-const RRZE_WP_VERSION = '5.3';
+const RRZE_PHP_VERSION = '7.4';
+const RRZE_WP_VERSION = '6.0';
 
-const RRZE_PLUGIN_FILE = __FILE__;
-
-// Automatische Laden von Klassen.
+/**
+ * SPL Autoloader (PSR-4).
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
 spl_autoload_register(function ($class) {
     $prefix = __NAMESPACE__;
-    $base_dir = __DIR__ . '/includes/';
+    $baseDir = __DIR__ . '/includes/';
 
     $len = strlen($prefix);
     if (strncmp($prefix, $class, $len) !== 0) {
         return;
     }
 
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    $relativeClass = substr($class, $len);
+    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
 
     if (file_exists($file)) {
         require $file;
     }
 });
 
-// Registriert die Plugin-Funktion, die bei Aktivierung des Plugins ausgeführt werden soll.
+// Register plugin hooks.
 register_activation_hook(__FILE__, __NAMESPACE__ . '\activation');
-// Registriert die Plugin-Funktion, die ausgeführt werden soll, wenn das Plugin deaktiviert wird.
 register_deactivation_hook(__FILE__, __NAMESPACE__ . '\deactivation');
-// Wird aufgerufen, sobald alle aktivierten Plugins geladen wurden.
+
 add_action('plugins_loaded', __NAMESPACE__ . '\loaded');
 
 /**
- * Einbindung der Sprachdateien.
+ * Loads a plugin’s translated strings.
  */
-function load_textdomain() {
-    load_plugin_textdomain('fau-oembed', false, sprintf('%s/languages/', dirname(plugin_basename(__FILE__))));
+function loadTextdomain()
+{
+    load_plugin_textdomain('fau-oembed', false, dirname(plugin_basename(__FILE__)) . '/languages');
 }
 
 /**
- * Wird durchgeführt, nachdem das Plugin aktiviert wurde.
+ * System requirements verification.
+ * @return string Return an error message.
  */
-function activation() {
-    // Sprachdateien werden eingebunden.
-    load_textdomain();
-
-    // Überprüft die minimal erforderliche PHP- u. WP-Version.
-    system_requirements();
-
-}
-
-/**
- * Wird durchgeführt, nachdem das Plugin deaktiviert wurde.
- */
-function deactivation() {
-    // Hier können die Funktionen hinzugefügt werden, die
-    // bei der Deaktivierung des Plugins aufgerufen werden müssen.
-    // Bspw. wp_clear_scheduled_hook, flush_rewrite_rules, etc.
-    
-    // TODO :))
-}
-
-/**
- * Überprüft die minimal erforderliche PHP- u. WP-Version.
- */
-function system_requirements() {
+function systemRequirements(): string
+{
+    global $wp_version;
+    // Strip off any -alpha, -RC, -beta, -src suffixes.
+    list($wpVersion) = explode('-', $wp_version);
+    $phpVersion = phpversion();
     $error = '';
-
-    if (version_compare(PHP_VERSION, RRZE_PHP_VERSION, '<')) {
-        /* Übersetzer: 1: aktuelle PHP-Version, 2: erforderliche PHP-Version */
-        $error = sprintf(__('Your server is running PHP version %1$s. Please upgrade at least to PHP version %2$s.', 'fau-oembed'), PHP_VERSION, RRZE_PHP_VERSION);
+    if (!is_php_version_compatible(RRZE_PHP_VERSION)) {
+        $error = sprintf(
+            /* translators: 1: Server PHP version number, 2: Required PHP version number. */
+            __('The server is running PHP version %1$s. The Plugin requires at least PHP version %2$s.', 'fau-oembed'),
+            $phpVersion,
+            RRZE_PHP_VERSION
+        );
+    } elseif (!is_wp_version_compatible(RRZE_WP_VERSION)) {
+        $error = sprintf(
+            /* translators: 1: Server WordPress version number, 2: Required WordPress version number. */
+            __('The server is running WordPress version %1$s. The Plugin requires at least WordPress version %2$s.', 'fau-oembed'),
+            $wpVersion,
+            RRZE_WP_VERSION
+        );
     }
+    return $error;
+}
 
-    if (version_compare($GLOBALS['wp_version'], RRZE_WP_VERSION, '<')) {
-        /* Übersetzer: 1: aktuelle WP-Version, 2: erforderliche WP-Version */
-        $error = sprintf(__('Your Wordpress version is %1$s. Please upgrade at least to Wordpress version %2$s.', 'fau-oembed'), $GLOBALS['wp_version'], RRZE_WP_VERSION);
-    }
-
-    // Wenn die Überprüfung fehlschlägt, dann wird das Plugin automatisch deaktiviert.
-    if (!empty($error)) {
-        deactivate_plugins(plugin_basename(__FILE__), false, true);
-        wp_die($error);
+/**
+ * Activation callback function.
+ */
+function activation()
+{
+    loadTextdomain();
+    if ($error = systemRequirements()) {
+        deactivate_plugins(plugin_basename(__FILE__));
+        wp_die(
+            sprintf(
+                /* translators: 1: The plugin name, 2: The error string. */
+                __('Plugins: %1$s: %2$s', 'fau-oembed'),
+                plugin_basename(__FILE__),
+                $error
+            )
+        );
     }
 }
 
 /**
- * Wird durchgeführt, nachdem das WP-Grundsystem hochgefahren
- * und alle Plugins eingebunden wurden.
+ * Deactivation callback function.
  */
-function loaded() {
-    // Sprachdateien werden eingebunden.
-    load_textdomain();
-
-    // Hauptklasse (Main) wird instanziiert.
-    new Main();
+function deactivation()
+{
+    // Nothing to do here.
 }
 
+/**
+ * Instantiate Plugin class.
+ * @return object Plugin
+ */
+function plugin()
+{
+    static $instance;
+    if (null === $instance) {
+        $instance = new Plugin(__FILE__);
+    }
+    return $instance;
+}
+
+/**
+ * Execute on 'plugins_loaded' API/action.
+ * @return void
+ */
+function loaded()
+{
+    loadTextdomain();
+    plugin()->loaded();
+    if ($error = systemRequirements()) {
+        add_action('admin_init', function () use ($error) {
+            if (current_user_can('activate_plugins')) {
+                $pluginData = get_plugin_data(plugin()->getFile());
+                $pluginName = $pluginData['Name'];
+                $tag = is_plugin_active_for_network(plugin()->getBaseName()) ? 'network_admin_notices' : 'admin_notices';
+                add_action($tag, function () use ($pluginName, $error) {
+                    printf(
+                        '<div class="notice notice-error"><p>' .
+                            /* translators: 1: The plugin name, 2: The error string. */
+                            __('Plugins: %1$s: %2$s', 'fau-oembed') .
+                            '</p></div>',
+                        esc_html($pluginName),
+                        esc_html($error)
+                    );
+                });
+            }
+        });
+        return;
+    }
+    new Main;
+}
